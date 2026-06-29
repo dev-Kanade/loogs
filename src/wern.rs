464292ee4d36ec45bcdccca56be77a7f27094add
奏 <kanade.miyachi@gmail.com>
2026-06-29 21:14:55 +0900

    警告

---
+++ src/wern.rs
@@ -0,0 +1,12 @@
+#[macro_export]
+macro_rules! wern {
+    ($x:expr)=>{
+        {
+            use colored::Colorize;
+            let now = chrono::Local::now();
+            let formatted_time = now.format("%Y-%m-%d %H:%M:%S").to_string();
+            let info = "[WERN]";
+            println!("{} {}:{}",info.yellow(),formatted_time.yellow(),$x);
+        }
+    }
+}
