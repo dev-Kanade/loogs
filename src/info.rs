a57c077c50d80e74e9b6a2751bb8f335743348f5
奏 <kanade.miyachi@gmail.com>
2026-06-29 21:02:52 +0900

    読み込む

--- src/info.rs
+++ src/info.rs
@@ -1,6 +1,7 @@
 #[macro_export]
 macro_rules! info {
     ($x:expr)=>{
+        use colored::Colorize;
         let now = chrono::Local::now();
         let formatted_time = now.format("%Y-%m-%d %H:%M:%S").to_string();
 
