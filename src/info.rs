dacf0d6bd8705b08bdd54fd040d3f199722924ce
奏 <kanade.miyachi@gmail.com>
2026-06-29 21:01:16 +0900

    フルパスに修正

--- src/info.rs
+++ src/info.rs
@@ -1,7 +1,7 @@
 #[macro_export]
 macro_rules! info {
     ($x:expr)=>{
-        let now = Local::now();
+        let now = chrono::Local::now();
         let formatted_time = now.format("%Y-%m-%d %H:%M:%S").to_string();
 
         println!("{}[INF]:{}",formatted_time,$x);
