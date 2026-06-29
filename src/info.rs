bff0f3384b83332545dc717595f1480a2627ba3d
奏 <kanade.miyachi@gmail.com>
2026-06-29 20:55:08 +0900

    出力まで各

--- src/info.rs
+++ src/info.rs
@@ -4,6 +4,9 @@ use chrono::Local;
 #[macro_export]
 macro_rules! info {
     (info:String)=>{
+        let now = Local::now();
+        let formatted_time = now.format("%Y-%m-%d %H:%M:%S").to_string();
 
+        println!("{}[INF]:{}",formatted_time,info);
     }
 }
