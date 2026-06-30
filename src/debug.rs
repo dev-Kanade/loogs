92b20ce104d183445586a398744006681484f13e
dev-kanade <kanade.miyachi@gmail.com>
2026-06-30 00:43:02 +0000

    クレートをフルパスで読み込む

--- src/debug.rs
+++ src/debug.rs
@@ -2,7 +2,7 @@
 macro_rules! debug {
     ($x:expr)=>{
         {
-            use colored::Colorize;
+            use $crate::loogs::colored::Colorize;
             let now = chrono::Local::now();
             let formatted_time = now.format("%Y-%m-%d %H:%M:%S").to_string();
             let info = "[DBUG]";
--- src/error.rs
+++ src/error.rs
@@ -2,7 +2,7 @@
 macro_rules! err {
     ($x:expr)=>{
         {
-            use colored::Colorize;
+            use $crate::loogs::colored::Colorize;
             let now = chrono::Local::now();
             let formatted_time = now.format("%Y-%m-%d %H:%M:%S").to_string();
             let types = "[EROR]";
